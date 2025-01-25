e5d94d6ceabfa962835331107ccca858d25e2b88
Navneet Gupta <133762095+Navneetg2003@users.noreply.github.com>
2025-01-25 22:43:59 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skill from "./components/skill";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return(
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Skill />
+        <Work />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
